c490a15818aac8643e02a50aaa61a721e5d4c0ba
lucieklgh <luciekl@hotmail.com>
2016-05-08 21:43:57 +0000

    nitrous-backup at Sun May  8 21:43:57 UTC 2016

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+    puts "squawk!"
+    parrot
+end
+
+def parrot(phrase="parrot")
+    puts "phrase"
+end
+
+parrot("pretty_bird")
